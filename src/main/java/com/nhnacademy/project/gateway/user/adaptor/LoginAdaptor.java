92e565ea9d0c1fc3fcfd3f2e5e76f9139d6d2be1
jjunho50 <jjunho50@gmail.com>
2024-02-25 23:07:32 +0900

    메인 브랜치 그레이트 리셋
    asdfasdf

--- src/main/java/com/nhnacademy/project/gateway/user/adaptor/LoginAdaptor.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.project.gateway.user.adaptor;
-
-import com.nhnacademy.project.gateway.user.domain.LoginDto;
-import com.nhnacademy.project.gateway.user.domain.UserDto;
-
-public interface LoginAdaptor {
-    UserDto matches(LoginDto loginDto);
-
-}
